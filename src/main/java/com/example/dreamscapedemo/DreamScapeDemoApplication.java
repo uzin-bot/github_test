1286afa1522e04ed5080a801e8b7ee01da8ed895
jeongyun kim <hhihaesuli3@gmail.com>
2025-11-03 09:33:43 +0900

    Init project structure

---
+++ src/main/java/com/example/dreamscapedemo/DreamScapeDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.dreamscapedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamScapeDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DreamScapeDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/dreamscapedemo/DreamScapeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamscapedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamScapedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
